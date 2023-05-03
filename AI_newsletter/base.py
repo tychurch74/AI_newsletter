d032bcb8ddb9e654631e1cc75ef3ecdd1d16320e
Ty Church <tychurch74@gmail.com>
2023-05-02 22:53:09 -0400

    update designs

--- AI_newsletter/base.py
+++ AI_newsletter/base.py
@@ -8,7 +8,11 @@ To-do:
     * Add DALLE image generation to create splash images
     * Cleanup code
 """
-from utils.date_info import get_seven_days_ago_date, is_today_monday, convert_todays_date
+from utils.date_info import (
+    get_seven_days_ago_date,
+    is_today_monday,
+    convert_todays_date,
+)
 from scrapers.arxiv_scraper import get_arxiv_papers
 from scrapers.github_scraper import get_github_repos
 from scrapers.twitter_scraper import format_twitter_results
@@ -16,31 +20,43 @@ from scrapers.news_scraper import get_top_10_ai_news
 from writers_room.chatGPT_writer import research_paper_writer
 from formatters.newsletter import newsletter_gen
 
+"""
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/LRJ9wnf/logo.png" alt="logo" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/M1tF51W/icon-facebook.png" alt="icon-facebook" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/Qbf05vS/icon-twitter.png" alt="icon-twitter" border="0"></a>
+"""
 
 last_week_date = get_seven_days_ago_date()
 todays_date = convert_todays_date()
 
 
-def research_papers(testing = False,search_term_1="artificial intelligence", search_term_2="natural language processing", max_results=5):
+def research_papers(
+    testing=False,
+    search_term_1="artificial intelligence",
+    search_term_2="natural language processing",
+    max_results=5,
+):
     pdf_filenames, result_pdf_dict = get_arxiv_papers(
         search_term_1, search_term_2, max_results=max_results
     )
     papers = []
     if testing:
-        papers = [{
-            "title": "title",
-            "date": "date",
-            "authors": "authors",
-            "article": "article",
-            "link": "link",
-        },
-        {
-            "title": "title2",
-            "date": "date2",
-            "authors": "authors2",
-            "article": "article2",
-            "link": "link2",
-        }]
+        papers = [
+            {
+                "title": "title",
+                "date": "date",
+                "authors": "authors",
+                "article": "article",
+                "link": "link",
+            },
+            {
+                "title": "title2",
+                "date": "date2",
+                "authors": "authors2",
+                "article": "article2",
+                "link": "link2",
+            },
+        ]
     else:
         for result in result_pdf_dict:
             title = result["title"]
@@ -78,17 +94,19 @@ def github_repos(date):
     return top_repos
 
 
-def top_tweets(testing = True):
+def top_tweets(testing=True):
     if testing:
-        top_tweets = [{"text": "Test text","handle": "@test handle","url": "/test/url"}, 
-                      {"text": "Test text2","handle": "@test handle2","url": "/test/url2"}]
+        top_tweets = [
+            {"text": "Test text", "handle": "@test handle", "url": "/test/url"},
+            {"text": "Test text2", "handle": "@test handle2", "url": "/test/url2"},
+        ]
     else:
         try:
             top_tweets = format_twitter_results("AI")
         except:
             top_tweets = ["Twitter scrape failed"]
     return top_tweets
-    
+
 
 ai_news = get_top_10_ai_news()
 newsletter_gen(todays_date, research_papers(), github_repos(last_week_date), ai_news)
--- AI_newsletter/writers_room/image_gen.py
+++ AI_newsletter/writers_room/image_gen.py
@@ -4,42 +4,19 @@ from pathlib import Path
 from base64 import b64decode
 import openai
 
-PROMPT = "An eco-friendly computer from the 90s in the style of vaporwave"
-DATA_DIR = Path.cwd() / "responses"
-
-DATA_DIR.mkdir(exist_ok=True)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def image_generator(prompt, n=1, size="256x256", response_format="json"):
+def image_generator(prompt, n=1, size="256x256"):
     response = openai.Image.create(
         prompt=prompt,
         n=n,
         size=size,
-        response_format=response_format,
     )
-    response = image_generator(PROMPT, n=10)
-
-    file_name = DATA_DIR / f"{PROMPT[:5]}-{response['created']}.json"
-
-    with open(file_name, mode="w", encoding="utf-8") as file:
-        json.dump(response, file)
-        return response
+    img_url = response["data"][0]["url"]
+    return img_url
 
 
-file_name = "An ec-1683070917.json"
-DATA_DIR = Path.cwd() / "responses"
-JSON_FILE = DATA_DIR / file_name
-IMAGE_DIR = Path.cwd() / "images" / JSON_FILE.stem
-
-IMAGE_DIR.mkdir(parents=True, exist_ok=True)
-
-with open(JSON_FILE, mode="r", encoding="utf-8") as file:
-    response = json.load(file)
-
-for index, image_dict in enumerate(response["data"]):
-    image_data = b64decode(image_dict["b64_json"])
-    image_file = IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png"
-    with open(image_file, mode="wb") as png:
-        png.write(image_data)
+prompt = "A picture of a messy desk with a 90s era computer on the desk in the style of vaporwave"
+print(image_generator(prompt))

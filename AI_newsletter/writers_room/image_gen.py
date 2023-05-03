effb9cfa05d01a6ed8c32a74cadca37b19ffff4a
Tyler Church <tychurch74@gmail.com>
2023-05-03 16:38:16 -0400

    fix:img gen prompt length

--- AI_newsletter/writers_room/image_gen.py
+++ AI_newsletter/writers_room/image_gen.py
@@ -17,11 +17,11 @@ openai_api_key = os.environ["OPENAI_API_KEY"]
 
 img_style = "vaporwave"
 
-prompt_messages = [{"role": "system", "content": f"Writer is a large language model trained by OpenAI that writes prompt-based image descriptions in the style of {img_style} in order to generate random images for a newsletter about artificial intelligence, machine learning and general computing. Your response should only include the generated image description and not contain any additional content or context. Your generated image description will be fed directly to a text to image generator (OpenAI's Dall-e 2)."}]
+prompt_messages = [{"role": "system", "content": f"Writer is a large language model trained by OpenAI that writes prompt-based image descriptions in the style of {img_style} in order to generate random images for a newsletter about artificial intelligence, machine learning and general computing. Make sure your response is under 375 characters in length. Your response should only include the generated image description and not contain any additional content or context. Your generated image description will be fed directly to a text to image generator (OpenAI's Dall-e 2)."}]
 
 def img_prompt_writer(img_style):
     openai.api_key = openai_api_key
-    text = f"Create a detailed image prompt to generate a random image in the style of {img_style} to be used in a newsletter about artificial intelligence, machine learning, and general computing."
+    text = f"Create a detailed image prompt to generate a random image in the style of {img_style} to be used in a newsletter about artificial intelligence, machine learning, and general computing. The image prompt should be under 375 characters in length."
     prompt_messages.append({"role": "user", "content": text})
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt_messages)
     prompt_content = completion.choices[0].message.content
